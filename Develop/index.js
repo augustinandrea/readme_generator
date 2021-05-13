bbd7da8a4af30e3213e6c793da62dc43725ec2de
augustinandrea <augustin.andrea@gmail.com>
2021-05-13 14:29:56 -0700

    Added all the questions section. Added how the file name should be. Wrote the writeFile.

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,81 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+// Install the `inquirer` dependency HERE
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+
+    {   name: "Project Title",
+        message: "Write the name of your project title",
+        type: "input"
+    },
+
+    {  name: "Description",
+       message: "Write a detailed explanation of what your project is and does: ",
+       type: "input"
+    },
+
+    {  name: "Table of Contents",
+       message: "Create a Table of Contents for all the sections needed: ",
+       type: "input"
+    },
+    
+    {   name: "Installation",
+        message: "How do you install your project?",
+        type: "input"
+    },
+
+    {   name: "Usage",
+        message: "How does your project work?",
+        type: "input"
+    },
+
+    {   name: "License",
+        message: "Please input the license for the project: ",
+        type: "input"
+    },
+
+    {   name: "Contributing",
+        meesage: "Who can contribute and how?",
+        type: "input"
+    },
+
+    {   name: "Tests",
+        message: "What are some tests for your project? ",
+        type: "input"
+    },
+
+    {   name: "Questions",
+        message: "Any questions needed for the project? ",
+        type: "input"
+    }
+
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    
+    fileName = data.name
+    .toUpperCase()
+    .split(' ')   
+    .join('') + '.md';
+
+    fs.writeFile( fileName, JSON.stringify(data, null, '\t'), function(err) {
+        if (err) {
+          return console.log(err);
+        }
+    });
+
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+    .then( writeToFile() );
+
+}
 
 // Function call to initialize app
 init();
