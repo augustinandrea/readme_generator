1e1d0b787c1b0478f6c4941d4bc5f119876f3917
augustinandrea <augustin.andrea@gmail.com>
2021-05-17 21:40:36 -0700

    I just added some stuff to the README and added the copyright symbol

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -123,7 +123,7 @@ function renderLicenseSection(license, username) {
   switch (license) {
 
     case "Apache 2.0":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright(c) [${d.getFullYear()}] [${username}] \n
 
       Licensed under the Apache License, Version 2.0 (the "License");
       you may not use this file except in compliance with the License.
@@ -139,7 +139,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "Eclipse":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
 
         Licensed under the Eclipse License, Version 2.0 (the "License");
         you may not use this file except in compliance with the License.
@@ -155,7 +155,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "GNU GPL v3":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
         
         This program is free software: you can redistribute it and/or modify
         it under the terms of the GNU General Public License as published by
@@ -172,7 +172,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "GNU GPL v2":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
         This program is free software; you can redistribute it and/or
         modify it under the terms of the GNU General Public License
         as published by the Free Software Foundation; either version 2
@@ -190,7 +190,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "GNU AGPL v3":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       This program is free software: you can redistribute it and/or modify
       it under the terms of the GNU Affero General Public License as
       published by the Free Software Foundation, either version 3 of the
@@ -207,7 +207,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "GNU LGPL v3":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       This program is free software: you can redistribute it and/or modify
       it under the terms of the GNU Affero General Public License as
       published by the Free Software Foundation, either version 3 of the
@@ -224,7 +224,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "GNU FDL v1.3":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       Permission is granted to copy, distribute and/or modify this document
       under the terms of the GNU Free Documentation License, Version 1.3
       or any later version published by the Free Software Foundation;
@@ -235,7 +235,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "ISC":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
       THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.      
@@ -243,7 +243,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "MIT":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 
       The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
@@ -253,7 +253,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "Mozilla":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
 
       THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.      
@@ -261,7 +261,7 @@ function renderLicenseSection(license, username) {
       break;
 
     case "Unlicense":
-      licenseSection = `Copyright(C) [${d.getFullYear()}] [${username}] \n
+      licenseSection = `Copyright© [${d.getFullYear()}] [${username}] \n
       This is free and unencumbered software released into the public domain.
 
       Anyone is free to copy, modify, publish, use, compile, sell, or
--- README.md
+++ README.md
@@ -1,5 +1,4 @@
 # README Generator Project
-  [![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)
 
   ## Description
   In this project I created a README Generator. The application starts and you are prompted with different questions related to different parts that should exists in a professional README. There is a license badge generated at the top and a license copyright section. There is a video showing an example of the process.
@@ -11,7 +10,6 @@
   * [Description](#Description)
   * [Installation](#Installation)
   * [Usage](#Usage)
-  * [License](#license)
   * [Contributing](#Contributing)
   * [Tests](#Tests)
   * [Questions](#Questions)
@@ -22,40 +20,6 @@
 
   ## Usage
   You start the project by going into the Develop folder. Then in the terminal, you type 'node index.js'.
-
-  
-  ## License 
-  Unlicense:
-  https://unlicense.org/
-  
-
-  Copyright(C) [2021] [augustinandrea] 
-
-      This is free and unencumbered software released into the public domain.
-
-      Anyone is free to copy, modify, publish, use, compile, sell, or
-      distribute this software, either in source code form or as a compiled
-      binary, for any purpose, commercial or non-commercial, and by any
-      means.
-      
-      In jurisdictions that recognize copyright laws, the author or authors
-      of this software dedicate any and all copyright interest in the
-      software to the public domain. We make this dedication for the benefit
-      of the public at large and to the detriment of our heirs and
-      successors. We intend this dedication to be an overt act of
-      relinquishment in perpetuity of all present and future rights to this
-      software under copyright law.
-      
-      THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-      EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-      MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-      IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
-      OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-      ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
-      OTHER DEALINGS IN THE SOFTWARE.
-      
-      For more information, please refer to <http://unlicense.org/>
-        
 
   ## Contributing
   Andrea Augustin
